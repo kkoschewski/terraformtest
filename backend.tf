49f76b237a3d64c6c9fed2ac6c9e6cba20bdfaa4
koschib <kevin.koschewski@materna.group>
2024-06-17 16:50:49 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "terraforminfrastructurebucket"
     key            = "./terraform.tfstate"
-    region         = "eu-central-3 (Berlin, Deutschland)"
     endpoint       = "https://s3.eu-central-3.ionoscloud.com/"  
     skip_credentials_validation = true
     skip_metadata_api_check     = true
