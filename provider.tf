a81138c5c4f93b773c1b8696bd9d01db2a7104dd
koschib <kevin.koschewski@materna.group>
2024-06-17 16:10:36 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,3 +7,15 @@ terraform {
   }
 }
 
+provider "aws" {
+  region                      = "de"
+  access_key                  =  ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key                  = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
+  # all these checks need to be skipped for an s3 external provider
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+  endpoints {
+     s3 = "https://s3.eu-central-3.ionoscloud.com/"
+  }
+}
