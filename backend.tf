5f2dead7e338e5a67aed30ae9d865c1e0dbd3d26
koschib <kevin.koschewski@materna.group>
2024-06-17 17:23:24 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,8 +4,6 @@ terraform {
     key                         = "terraform.tfstate"
     endpoint                    = "https://s3.eu-central-3.ionoscloud.com/"
     region                      = "de"
-    access_key                  = var.aws_access_key
-    secret_key                  = var.aws_secret_key
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
