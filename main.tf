5315fb3d3ce2622abd9f2ef671569b9ffef7b485
koschib <kevin.koschewski@materna.group>
2024-06-17 15:50:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-resource "ionoscloud_datacenter" "datacenter" {
-  name                    = var.datacenter_name
-  location                = "de/txl"
-  description             = var.datacenter_description
-}
-
-
 
 
 ## End of snippet
