659106321b13f925181c955a27456c9b7032035f
koschib <kevin.koschewski@materna.group>
2024-06-18 16:17:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ terraform {
     key            = "tfcluster/terraform.tfstate"
     region         = "de"          # Region ist optional für Ionos Cloud
     endpoint       = "s3.eu-central-1.ionoscloud.com"  # Beispiel für Ionos Cloud
-    access_key     = var.AWS_ACCESS_KEY_ID
-    secret_key     = var.AWS_SECRET_ACCESS_KEY  
+    access_key     = "${AWS_ACCESS_KEY_ID}"
+    secret_key     = "${AWS_SECRET_ACCESS_KEY}"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
   }
