f795cc216bf0d00b09e3e459168577f59446d99c
koschib <kevin.koschewski@materna.group>
2024-06-21 10:55:22 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,8 +4,6 @@ terraform {
     region                      = "de"
     bucket                      = "terraforminfrastructurebucket"
     key                         = "terraforn/terraform.tfstate"
-    access_key = var.access_key
-    secret_key = var.secret_key
     skip_credentials_validation = true
     skip_region_validation      = true
   }
