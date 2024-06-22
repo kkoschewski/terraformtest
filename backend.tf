079233d5fdb0256a97af239d8f2a79918c05d2c3
koschib <98027016+koschib@users.noreply.github.com>
2024-06-22 22:51:45 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,8 +3,14 @@ terraform {
     endpoint                    = "https://s3.eu-central-3.ionoscloud.com/"
     region                      = "de"
     bucket                      = "terraforminfrastructurebucket"
-    key                         = "terraforn/terraform.tfstate"
+    key                         = "terraform.tfstate"
     skip_credentials_validation = true
+    skip_get_ec2_platforms      = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_zone_validation        = true
+    
+  
   }
 }
