a5acc4754e3d00f6b26193cbffc3505b3bc66a02
koschib <98027016+koschib@users.noreply.github.com>
2024-06-22 22:58:06 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,12 +4,10 @@ terraform {
     region                      = "de"
     bucket                      = "terraforminfrastructurebucket"
     key                         = "terraform.tfstate"
-    skip_credentials_validation = true
-    skip_get_ec2_platforms      = true
-    skip_requesting_account_id  = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    skip_zone_validation        = true
+    encrypt = true
+    dynamodb_table = "mycomponents_tf_lockid"
+    skip_region_validation = true
+
     
   
   }
